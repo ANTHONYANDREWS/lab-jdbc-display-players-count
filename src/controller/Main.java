72ce5a27e430541fbedfb2af7ade57bede81308d
ANTHONYANDREWS <30433910+ANTHONYANDREWS@users.noreply.github.com>
2020-03-22 20:21:50 +0530

    My work

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,17 @@
+package controller;
 
+import java.util.TreeMap;
+
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main{
+	
+	SkillDAO skilldao = new SkillDAO();
+	
+	 TreeMap<Skill,Integer> sc = new TreeMap<Skill,Integer>();
+	 
+	 sc = skilldao.skillCount();
+		System.out.println("Id"+"\t"+"name"+"\t"+"count");
+
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,41 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.TreeMap;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO{
+	public TreeMap<Skill,Integer> skillCount() throws SQLException{
+		
+		Connection con = null;
+		try {
+			con = ConnectionManager.getConnection();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+			Statement statement = con.createStatement();
+	
+		
+		int batsmen =0;
+		int bowler =0;
+		int allrounder = 0;
+		int wk_batsmen = 0;
+		
+		ResultSet resultset = statement.executeQuery("Select * from Player");
+		
+		while (resultset.next()) {
+			
+		}
+		
+		return null;
+		
+	}
+}
 
